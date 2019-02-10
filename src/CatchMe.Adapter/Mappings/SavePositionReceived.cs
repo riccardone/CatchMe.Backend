70dcee28295ce8ee16bb5b697a53ef42a30e23d1
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2019-02-10 18:30:31 +0000

    implementing saving position handling (wip)

---
+++ src/CatchMe.Adapter/Mappings/SavePositionReceived.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Evento;
+using Newtonsoft.Json;
+
+namespace CatchMe.Adapter.Mappings
+{
+    public class SavePositionReceived : Command
+    {
+        public SavePositionReceived(string bodyAsJson, string metadataAsJson)
+        {
+            var body = JsonConvert.DeserializeObject<dynamic>(bodyAsJson);
+            var metadata = JsonConvert.DeserializeObject<IDictionary<string, string>>(metadataAsJson);
+
+            Latitude = body.ownerId.Value;
+            Longitude = body.friendId.Value;
+            // TODO
+            Metadata = metadata;
+        }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public IDictionary<string, string> Metadata { get; }
+    }
+}
